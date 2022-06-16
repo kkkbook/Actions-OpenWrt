a9802ad473b4bbbc53599182d4e695cbee037be2
kkkbook <42238629+kkkbook@users.noreply.github.com>
2022-06-16 11:01:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,7 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git nat6 https://github.com/Ausaci/luci-app-nat6-helper' feeds.conf.default
 
